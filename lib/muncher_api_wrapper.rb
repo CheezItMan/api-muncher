a5debb45571dcd1e0d435e365a01aadfad5d0d94
Elle Vargas <vargas.elle@gmail.com>
2016-11-04 15:48:31 -0700

    show recipe is now correctly using httparty response object

--- lib/muncher_api_wrapper.rb
+++ lib/muncher_api_wrapper.rb
@@ -11,9 +11,9 @@ class MuncherApiWrapper
     app_key = APP_KEY if app_key == nil
     url = BASE_URL + "search?" + "&app_id=#{app_id}" + "&app_key=#{app_key}" + "&q=#{query}" + "&from=0&to=100"
     data = HTTParty.get(url)
-    recipes = []
     # check to see if url is set up correctly to begin troubleshooting
 
+    recipes = []
     if data["hits"].length > 0
       data["hits"].each do |recipe|
         wrapper = Recipe.new(recipe["recipe"]["label"], recipe["recipe"]["image"], recipe["recipe"]["url"], recipe["recipe"]["dietLabels"], recipe["recipe"]["healthLabels"], recipe["recipe"]["ingredientLines"], recipe["recipe"]["uri"])
@@ -27,8 +27,23 @@ class MuncherApiWrapper
   def self.show_recipe(uri)
     url = BASE_URL + "search?" + "&r=#{URI.encode(uri)}"
     response = HTTParty.get(url)
-    return response.parsed_response
-  end
 
+    recipes = []
+
+    response.parsed_response.each do |recipe|
+      wrapper = Recipe.new(
+        recipe["label"],
+        recipe["image"],
+        recipe["url"],
+        recipe["dietLabels"],
+        recipe["healthLabels"],
+        recipe["ingredientLines"],
+        recipe["uri"])
+
+      recipes << wrapper
+    end
+    return recipes
+
+  end
 
 end
